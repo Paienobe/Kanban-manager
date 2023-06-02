b583f365c9a9cdbee990b0356b7fade90a4bbafb
Paienobe <paienobe@gmail.com>
2023-06-02 12:14:11 +0100

    navbar ui

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect, useRef } from "react";
 import { useGlobalContext } from "../../context/globalContext";
 import mobileLogo from "../../assets/logo-mobile.svg";
+import desktopLightLogo from "../../assets/logo-light.svg";
+import desktopDarkLogo from "../../assets/logo-dark.svg";
 import addIcon from "../../assets/icon-add-task-mobile.svg";
 import moreIcon from "../../assets/icon-vertical-ellipsis.svg";
 import downIcon from "../../assets/icon-chevron-down.svg";
@@ -21,11 +23,17 @@ const Navbar = ({
   showBoardForm,
   setShowBoardForm,
 }: Props) => {
-  const { currentBoard, setDeleteItem, setShowDeleteModal, setEditBoard } =
-    useGlobalContext()!;
+  const {
+    currentBoard,
+    setDeleteItem,
+    setShowDeleteModal,
+    setEditBoard,
+    theme,
+  } = useGlobalContext()!;
   const currentBoardName = currentBoard?.name || "No Board Found";
   const [showOptions, setShowOptions] = useState(false);
   const optionsRef = useRef<HTMLDivElement>(null);
+  const desktopLogo = theme === "dark" ? desktopLightLogo : desktopDarkLogo;
 
   useEffect(() => {
     document.addEventListener("mousedown", (e) =>
@@ -45,14 +53,18 @@ const Navbar = ({
   }, [showBoardForm]);
 
   return (
-    <div className="p-4 py-6 bg-lightTiles dark:bg-darkTiles flex items-center justify-between fixed top-0 left-0 right-0 border-subtextColor border border-x-0 border-t-0 border-opacity-30 transition-[background] duration-300 ease-in-out">
-      <div className="w-[10%]">
+    <div className="p-4 py-6 md:py-0 bg-lightTiles dark:bg-darkTiles flex items-center justify-between fixed top-0 left-0 right-0 border-subtextColor border border-x-0 border-t-0 border-opacity-30 transition-[background] duration-300 ease-in-out">
+      <div className="w-[10%] md:hidden">
         <img src={mobileLogo} alt="" />
       </div>
 
-      <div className="w-[70%] flex items-center">
+      <div className="hidden md:block w-[40%] border border-transparent border-r-subtextColor border-opacity-30 py-8">
+        <img src={desktopLogo} alt="" />
+      </div>
+
+      <div className="w-[70%] md:w-[30%] flex items-center">
         <h1
-          className="text-left text-lightModeTitle dark:text-darkModeTitle font-bold text-xl mr-2"
+          className="text-left text-lightModeTitle dark:text-darkModeTitle font-bold text-xl md:text-2xl mr-2"
           onClick={() => {
             setShowBoardsModal(true);
           }}
@@ -63,7 +75,7 @@ const Navbar = ({
           src={downIcon}
           className={`transition-all duration-300 ${
             showBoardsModal && "rotate-180"
-          }`}
+          } md:hidden`}
           alt=""
           onClick={() => {
             setShowBoardsModal(true);
@@ -71,13 +83,18 @@ const Navbar = ({
         />
       </div>
 
-      <div className="w-[20%] flex items-center justify-between">
+      <div className="w-[20%] md:w-[23%] flex items-center justify-between">
         <div
-          className="bg-purple p-2 w-[70%] rounded-xl flex items-center justify-center"
+          className="bg-purple p-2 w-[70%] md:hidden rounded-xl flex items-center justify-center"
           onClick={() => setShowTaskForm(true)}
         >
           <img src={addIcon} alt="" />
         </div>
+
+        <button className="hidden md:block w-[85%] bg-purple text-white text-sm py-2 rounded-full font-semibold">
+          +Add New Task
+        </button>
+
         <img src={moreIcon} alt="" onClick={() => setShowOptions(true)} />
         {showOptions && (
           <div
